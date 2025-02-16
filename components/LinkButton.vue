3c85bd962e360707c3baa4c7a6757d7cd81162f0
Matt Millican <matt@mattmillican.comn>
2025-02-16 00:11:56 -0600

    Move comments

--- components/LinkButton.vue
+++ components/LinkButton.vue
@@ -13,10 +13,6 @@
 
     {{ props.link.name }}
   </a>
-
-  <!-- Hack to make sure that TW compiles these classes since they are referenced dynamically -->
-  <!-- <div class="bg-facebook bg-discord bg-instagram bg-github bg-linkedin bg-threads bg-mvr bg-m2 bg-railops" /> -->
-   <!-- <div class="hover:bg-facebook/70 hover:bg-discord/70 hover:bg-instagram/70 hover:bg-github/70 hover:bg-linkedin/70 hover:bg-threads/70 hover:bg-mvr/70 hover:bg-m2/70 hover:bg-railops/70" /> -->
 </template>
 
 <script setup lang="ts">
--- components/LinksContainer.vue
+++ components/LinksContainer.vue
@@ -2,6 +2,10 @@
   <div class="max-w-sm mx-auto text-white text-center flex flex-col space-y-6">
     <LinkButton v-for="(link, idx) in data?.links" :key="idx" :link="link" />
   </div>
+
+  <!-- Hack to make sure that TW compiles these classes since they are referenced dynamically -->
+  <!-- <div class="bg-facebook bg-discord bg-instagram bg-github bg-linkedin bg-threads bg-mvr bg-m2 bg-railops" /> -->
+  <!-- <div class="hover:bg-facebook/70 hover:bg-discord/70 hover:bg-instagram/70 hover:bg-github/70 hover:bg-linkedin/70 hover:bg-threads/70 hover:bg-mvr/70 hover:bg-m2/70 hover:bg-railops/70" /> -->
 </template>
 
 <script setup lang="ts">
