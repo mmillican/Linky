9a5cbd1104fdeea1767dd31f098f34058bb11f2a
Matt Millican <matt@mattmillican.comn>
2025-02-16 00:23:02 -0600

    Add padding on mobile

--- components/LinksContainer.vue
+++ components/LinksContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-sm mx-auto text-white text-center flex flex-col space-y-4">
+  <div class="max-w-sm mx-auto px-4 sm:px-0 text-white flex flex-col space-y-4">
     <LinkButton v-for="(link, idx) in data?.links" :key="idx" :link="link" />
   </div>
 
